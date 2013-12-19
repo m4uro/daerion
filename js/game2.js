16692c5cab2d04d1d63188e8bc8e66da926135f6
Mauro <m4uro.m@gmail.com>
2013-12-19 16:37:05 -0300

    Added background

--- js/game2.js
+++ js/game2.js
@@ -1,24 +1,36 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1024, 656, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
     game.load.atlas('wizard', 'assets/wizard.png', 'assets/wizard.json');
+    game.load.image('fondo', 'assets/bosque_fondo.png');
+    game.load.image('arbol1', 'assets/bosque_arbol1.png');
+    game.load.image('arbol2', 'assets/bosque_arbol2.png');
+    game.load.image('arbol3', 'assets/bosque_arbol3.png');
+    game.load.image('arbol4', 'assets/bosque_arbol4.png');
 }
 
 var wizard;
 var move;
 var contador = 0;
 
+
 function create() {
-    wizard = game.add.sprite(100, 100, 'wizard');
-    game.stage.backgroundColor = '#555';
+    game.add.sprite(0, 0, 'fondo');
+    
+    game.add.sprite(210, 4, 'arbol2');
+    game.add.sprite(675, 2, 'arbol3');
+    wizard = game.add.sprite(100, 300, 'wizard');
+    game.add.sprite(12, 0, 'arbol1');
+    game.add.sprite(815, 0, 'arbol4');
+    //game.stage.backgroundColor = '#555';
     
     wizard.animations.add('attack', Phaser.Animation.generateFrameNames('Wizard', 0, 14, '', 4), 40, true, false);
     wizard.animations.add('idle', Phaser.Animation.generateFrameNames('Wizard', 15, 44, '', 4), 30, true, false);
     wizard.animations.add('move', Phaser.Animation.generateFrameNames('Wizard', 45, 71, '', 4), 30, true, false);
 
-    //wizard.animations.play('move', null, true);
+    wizard.animations.play('move', null, true);
 
-    //move = true;
+    move = true;
     
     wizard.inputEnabled = true;
 //    wizard.input.pixelPerfect = true;
@@ -47,7 +59,7 @@ function changeAnimation() {
 }
 
 function render() {
-    game.debug.renderSpriteInputInfo(wizard, 32, 32);
+//    game.debug.renderSpriteInputInfo(wizard, 32, 32);
 //    game.debug.renderSpriteBounds(wizard);
-    game.debug.renderPoint(wizard.input._tempPoint);
+//    game.debug.renderPoint(wizard.input._tempPoint);
 }
