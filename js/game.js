6666f8c6f2948ccf3e752b47a27b5041211aa0fe
Mauro <m4uro.m@gmail.com>
2014-01-10 05:48:10 -0300

    Added flipping. Fixed anchor point

--- js/game.js
+++ js/game.js
@@ -1,47 +1,150 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1024, 656, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    //game.load.atlas('wizard', 'assets/wizardStarling.png', 'assets/wizardStarling.xml', null, Phaser.Loader.TEXTURE_ATLAS_XML_STARLING);
-    game.load.atlas('wizard', 'assets/wizard.png', 'assets/wizard.json');
+    game.load.atlas('wizard', 'assets/prueba.png', 'assets/prueba.json');
+    game.load.atlas('wolf', 'assets/wolf.png', 'assets/wolf.json');
+    game.load.image('bg', 'assets/woods_bg.png');
+    game.load.image('tree1', 'assets/woods_tree1.png');
+    game.load.image('tree2', 'assets/woods_tree2.png');
+    game.load.image('tree3', 'assets/woods_tree3.png');
+    game.load.image('tree4', 'assets/woods_tree4.png');
+    game.load.image('sRing', 'assets/selection_ring.png');
+    game.load.image('buttonF', 'assets/button_fire.png');
+    game.load.image('buttonL', 'assets/button_lightning.png');
+    game.load.image('buttonP', 'assets/button_pause.png');
+    game.load.image('iconWizard', 'assets/icon_wizard.png');
+    game.load.bitmapFont('agency', 'assets/agency.png', 'assets/agency.xml');
 }
 
-var wizard;
-var move;
-var atck;
+var wizard, sRing;
+var move, bg;
+var contador = 0;
+var g;
+var text;
 
 function create() {
-    wizard = game.add.sprite(100, 100, 'wizard');
-    game.stage.backgroundColor = '#ff5';
     
-    wizard.animations.add('attack', ['Wizard0000', 'Wizard0001', 'Wizard0002', 'Wizard0003', 'Wizard0004', 'Wizard0005', 'Wizard0006', 'Wizard0007', 'Wizard0008', 'Wizard0009', 'Wizard0010', 'Wizard0011', 'Wizard0012', 'Wizard0013', 'Wizard0014'], 50, true, false);
-    wizard.animations.add('idle', ['Wizard0015', 'Wizard0016', 'Wizard0017', 'Wizard0018', 'Wizard0019', 'Wizard0020', 'Wizard0021', 'Wizard0022', 'Wizard0023', 'Wizard0024', 'Wizard0025', 'Wizard0026', 'Wizard0027', 'Wizard0028', 'Wizard0029', 'Wizard0030', 'Wizard0031', 'Wizard0032', 'Wizard0033', 'Wizard0034', 'Wizard0035', 'Wizard0036', 'Wizard0037', 'Wizard0038', 'Wizard0039', 'Wizard0040', 'Wizard0041', 'Wizard0042', 'Wizard0043', 'Wizard0044'], 30, true, false);
-    wizard.animations.add('move', ['Wizard0045', 'Wizard0046', 'Wizard0047', 'Wizard0048', 'Wizard0049', 'Wizard0050', 'Wizard0051', 'Wizard0052', 'Wizard0053', 'Wizard0054', 'Wizard0055', 'Wizard0056', 'Wizard0057', 'Wizard0058', 'Wizard0059', 'Wizard0060', 'Wizard0061', 'Wizard0062', 'Wizard0063', 'Wizard0064', 'Wizard0065', 'Wizard0066', 'Wizard0067', 'Wizard0068', 'Wizard0069', 'Wizard0070', 'Wizard0071'], 30, true, false);
+    bg = game.add.sprite(0, 0, 'bg');
+    game.add.sprite(210, 4, 'tree2');
+    game.add.sprite(675, 2, 'tree3');
+    sRing = game.add.sprite(0, 0, 'sRing');
+    wizard = game.add.sprite(100, 300, 'wizard');
+    wolf = game.add.sprite(300, 300, 'wolf');
+    game.add.sprite(12, 0, 'tree1');
+    game.add.sprite(815, 0, 'tree4');
+    
+    
+    //game.stage.backgroundColor = '#555';
+    
+    wizard.animations.add('attack', Phaser.Animation.generateFrameNames('Wizard', 0, 14, '', 4), 40, true, false);
+    wizard.animations.add('idle', Phaser.Animation.generateFrameNames('Wizard', 15, 44, '', 4), 30, true, false);
+    wizard.animations.add('move', Phaser.Animation.generateFrameNames('Wizard', 45, 71, '', 4), 30, true, false);
 
-    //bot.velocity.x = -100;
     wizard.animations.play('move', null, true);
-    wizard.input.start(0, false, true);
+    
+    wolf.animations.add('attack', Phaser.Animation.generateFrameNames('Wolf', 0, 21, '', 4), 30, true, false);
+    wolf.animations.add('move', Phaser.Animation.generateFrameNames('Wolf', 22, 37, '', 4), 30, true, false);
+    wolf.animations.add('idle', Phaser.Animation.generateFrameNames('Wolf', 38, 61, '', 4), 30, true, false);
+    
+    wolf.animations.play('move', null, true);
+   
     move = true;
-    atck = false;
+    
+    wizard.inputEnabled = true;
+//    wizard.input.pixelPerfect = true;
+    wizard.input.useHandCursor = true;
+    wizard.events.onInputDown.add(changeAnimation, this);
+    
+    wolf.inputEnabled = true;
+    wolf.input.useHandCursor = true;
+    
+    wizard.offsetX = -11;
+    wizard.offsetY = 0;
+    wolf.offsetX = 3;
+    wolf.offsetY = -10;
+
+    wizard.events.onInputDown.add(getSelectionRing);
+    wolf.events.onInputDown.add(getSelectionRing);
+    
+    //Drawing transparent rectangles for bottom interface:
+    g = game.add.graphics(0,0);
+    g.fillColor = 0x000000;
+    g.fillAlpha = 0.53;
+    g.drawRect(0, bg.height - 68, bg.width, 68);
+    g.fillColor = 0xffffff;
+    g.fillAlpha = 0.72;
+    g.drawRect(25, bg.height - 48, bg.width - 50, 28);
+    game.add.sprite(20, bg.height- 56, 'buttonP');
+    game.add.sprite(400, bg.height- 56, 'buttonF');
+    game.add.sprite(450, bg.height- 56, 'buttonL');
+    game.add.sprite(bg.width - 65, bg.height- 56, 'buttonP');
+    boton = game.add.sprite(bg.width- 96, 16 , 'iconWizard');
+    boton.inputEnabled = true;
+    boton.input.enableDrag();
+    
+    //Interface text:
+    text = game.add.bitmapText(100, bg.height - 50, 'Daerion', { font: '32px AgencyFB', align: 'center' });
+}
+
+function getSelectionRing(target) {
+    sRing.scale.x = target.width/80;
+    sRing.x = target.x + (target.width - sRing.width)/2 + target.offsetX;
+    sRing.y = target.y + target.height - sRing.height/2 + target.offsetY;
+    sRing.selected = target;
 }
 
 function update() {
-    if (wizard.input.justPressed(0, 30)) {
-        if (atck) {
-            //wizard.y += 21;
-            wizard.animations.play('idle', null, true);
-
-            atck = false;
-        } else {
-            //wizard.y -= 21;
-            wizard.animations.play('attack', null, true);
-
-            atck = true;
-        }
-    }
     if (move === true) {
         wizard.x += 1;
         if (wizard.x > game.width) {
             wizard.x = -20;
         }
+        if (sRing.selected === wizard) {
+//            sRing.x = wizard.x -8;
+            getSelectionRing(wizard);
+//            sRing.x = wizard.x + wizard.width/2 - 33;
+//            sRing.y = wizard.y + wizard.height - 12;
+//            sRing.y = wizard.y+72;
+        }
     }
+    wolf.x += 2;
+    if (wolf.x > game.width) {
+        wolf.x = -20;
+    }
+    if (sRing.selected === wolf) {
+            getSelectionRing(wolf);
+    }
+//    if (game.input.mousePointer.isDown)
+//    {
+//        //  400 is the speed it will move towards the mouse
+//        game.physics.moveToPointer(wizard, 100);
+//
+//        //  if it's overlapping the mouse, don't move any more
+//        if (Phaser.Rectangle.contains(wizard.body, game.input.x, game.input.y))
+//        {
+//            wizard.body.velocity.setTo(0, 0);
+//        }
+//    }
+//    else {
+//        wizard.body.velocity.setTo(0,0);
+//    }
+}
+
+function changeAnimation() {
+    var animations = ['attack', 'idle', 'move'];
+    contador = (contador + 1) % 3;
+    wizard.animations.play(animations[contador], null, true);
+    if (contador === 2) {
+        move = true;
+    } else {
+        move = false;
+    }
+}
+
+function render() {
+//    game.debug.renderSpriteInputInfo(boton, 32, 32);
+//    game.debug.renderSpriteBounds(wizard);
+//    game.debug.renderSpriteBounds(wolf);
+//    game.debug.renderPoint(wizard.input._tempPoint);
+    
 }
--- js/game2.js
+++ js/game2.js
@@ -9,22 +9,33 @@ function preload() {
     game.load.image('tree3', 'assets/woods_tree3.png');
     game.load.image('tree4', 'assets/woods_tree4.png');
     game.load.image('sRing', 'assets/selection_ring.png');
+    game.load.image('buttonF', 'assets/button_fire.png');
+    game.load.image('buttonL', 'assets/button_lightning.png');
+    game.load.image('buttonP', 'assets/button_pause.png');
+    game.load.image('iconWizard', 'assets/icon_wizard.png');
+    game.load.bitmapFont('agency', 'assets/agency.png', 'assets/agency.xml');
+    game.load.image('puntero', 'assets/pointer.png');
 }
 
 var wizard, sRing;
-var move;
+var move, bg;
 var contador = 0;
-
+var g;
+var text, puntero;
 
 function create() {
-    game.add.sprite(0, 0, 'bg');
+    
+    bg = game.add.sprite(0, 0, 'bg');
     game.add.sprite(210, 4, 'tree2');
     game.add.sprite(675, 2, 'tree3');
-    sRing = game.add.sprite(0,0,'sRing');
-    wizard = game.add.sprite(100, 300, 'wizard');
-    wolf = game.add.sprite(300, 300, 'wolf');
+    sRing = game.add.sprite(0, 0, 'sRing');
+    wizard = game.add.sprite(100, 380, 'wizard');
+    wolf = game.add.sprite(300, 400, 'wolf');
     game.add.sprite(12, 0, 'tree1');
     game.add.sprite(815, 0, 'tree4');
+    puntero = game.add.sprite(0,0, 'puntero');
+    puntero.anchor.setTo(0.5, 0.5);
+    
     //game.stage.backgroundColor = '#555';
     
     wizard.animations.add('attack', Phaser.Animation.generateFrameNames('Wizard', 0, 14, '', 4), 40, true, false);
@@ -45,49 +56,89 @@ function create() {
 //    wizard.input.pixelPerfect = true;
     wizard.input.useHandCursor = true;
     wizard.events.onInputDown.add(changeAnimation, this);
-    
+    //anchor exacto: (regPoint.x/sourceSize.w, regPoint.y/sourceSize.h)
+    wizard.anchor.setTo(0.3, 1); //so it flips around its middle
+    //wizard.anchor.setTo(0.2, 1);
     wolf.inputEnabled = true;
     wolf.input.useHandCursor = true;
+    wolf.anchor.setTo(0.54, 0.95);
     
-    wizard.offsetX = -11;
-    wizard.offsetY = 0;
-    wolf.offsetX = 3;
-    wolf.offsetY = -10;
+//    wizard.offsetX = 0;
+//    wizard.offsetY = 0;
+    wolf.offsetX = -3;
+    wolf.offsetY = -5;
 
     wizard.events.onInputDown.add(getSelectionRing);
     wolf.events.onInputDown.add(getSelectionRing);
+    
+    //Drawing transparent rectangles for bottom interface:
+    g = game.add.graphics(0,0);
+    g.fillColor = 0x000000;
+    g.fillAlpha = 0.53;
+    g.drawRect(0, bg.height - 68, bg.width, 68);
+    g.fillColor = 0xffffff;
+    g.fillAlpha = 0.72;
+    g.drawRect(25, bg.height - 48, bg.width - 50, 28);
+    game.add.sprite(20, bg.height- 56, 'buttonP');
+    game.add.sprite(400, bg.height- 56, 'buttonF');
+    game.add.sprite(450, bg.height- 56, 'buttonL');
+    game.add.sprite(bg.width - 65, bg.height- 56, 'buttonP');
+    boton = game.add.sprite(bg.width- 96, 16 , 'iconWizard');
+    boton.inputEnabled = true;
+    boton.input.enableDrag();
+    puntero.destroy();
+    //Interface text:
+    text = game.add.bitmapText(100, bg.height - 50, 'Daerion', { font: '32px AgencyFB', align: 'center' });
 }
 
 function getSelectionRing(target) {
-//    sRing.x = this.x - 8;
     sRing.scale.x = target.width/80;
-    sRing.x = target.x + (target.width - sRing.width)/2 + target.offsetX;
-//    sRing.y = this.y + 72;
-    sRing.y = target.y + target.height - sRing.height/2 + target.offsetY;
+//    sRing.x = target.x + (target.width - sRing.width)/2 + target.offsetX;
+//    sRing.y = target.y + target.height - sRing.height/2 + target.offsetY;
+    sRing.x = target.x - sRing.width/2 + (target.offsetX? target.offsetX: 0);
+    sRing.y = target.y - sRing.height/2 + (target.offsetY? target.offsetY: 0);
     sRing.selected = target;
 }
 
 function update() {
     if (move === true) {
-        wizard.x += 1;
+        wizard.x += 1 * wizard.scale.x;
         if (wizard.x > game.width) {
             wizard.x = -20;
         }
+        if (wizard.x < -20) {
+            wizard.x = bg.width;
+        }
         if (sRing.selected === wizard) {
-//            sRing.x = wizard.x -8;
             getSelectionRing(wizard);
-//            sRing.x = wizard.x + wizard.width/2 - 33;
-//            sRing.y = wizard.y + wizard.height - 12;
-//            sRing.y = wizard.y+72;
         }
     }
-    wolf.x += 2;
+    wolf.x += 2 * wizard.scale.x;
     if (wolf.x > game.width) {
         wolf.x = -20;
+    }
+    if (wolf.x < -20) {
+        wolf.x = bg.width;
     }
     if (sRing.selected === wolf) {
             getSelectionRing(wolf);
     }
+//    if (game.input.mousePointer.isDown)
+//    {
+//        //  400 is the speed it will move towards the mouse
+//        game.physics.moveToPointer(wizard, 100);
+//
+//        //  if it's overlapping the mouse, don't move any more
+//        if (Phaser.Rectangle.contains(wizard.body, game.input.x, game.input.y))
+//        {
+//            wizard.body.velocity.setTo(0, 0);
+//        }
+//    }
+//    else {
+//        wizard.body.velocity.setTo(0,0);
+//    }
+    puntero.x = wolf.x;
+    puntero.y = wolf.y;
 }
 
 function changeAnimation() {
@@ -99,10 +150,12 @@ function changeAnimation() {
     } else {
         move = false;
     }
+    wizard.scale.x *= -1; //flipped
+    wolf.scale.x *= -1;
 }
 
 function render() {
-//    game.debug.renderSpriteInputInfo(wizard, 32, 32);
+//    game.debug.renderSpriteInputInfo(boton, 32, 32);
 //    game.debug.renderSpriteBounds(wizard);
 //    game.debug.renderSpriteBounds(wolf);
 //    game.debug.renderPoint(wizard.input._tempPoint);
